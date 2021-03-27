948c6d28caa68cf22b795919e7e6c10873b55cc0
Christian López Barrón <chris.gfz@gmail.com>
2021-03-27 22:23:52 +1100

    add initial grill.easyedb package

--- chapter3/usd.py
+++ chapter3/usd.py
@@ -1,157 +1,59 @@
-import types
 import logging
 from pathlib import Path
 
-from grill import names
+from pxr import Usd, Sdf, Kind
 
-from pxr import UsdUtils, Usd, Sdf, Ar, Kind
+from grill import easyedb
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
-repo = Path(__file__).parent / "repo"
+
+dracula_root_id = easyedb.UsdFile.get_default(code='dracula')
+
+token = easyedb.repo.set(Path(__file__).parent / "repo")
 
 
-class UsdFile(names.CGAssetFile):
-    DEFAULT_SUFFIX = 'usda'
-
-
-dracula_root_id = UsdFile.get_default(code='dracula')
-logger.info(f"Repository path: {repo}")
+logger.info(f"Repository path: {easyedb.repo.get()}")
 logger.info(f"Stage identifier: {dracula_root_id}")
 
-
-def fetch_stage(root_id) -> Usd.Stage:
-    """For the given root layer identifier, get a corresponding stage.
-
-    If layer does not exist, it is created in the repository.
-
-    If a stage for the corresponding layer is found on the global cache, return it.
-    Otherwise open it, populate the cache and return it.
-
-    :param root_id:
-    :return:
-    """
-    rootf = UsdFile(root_id)
-    cache = UsdUtils.StageCache.Get()
-    resolver_ctx = Ar.DefaultResolverContext([str(repo)])
-    with Ar.ResolverContextBinder(resolver_ctx):
-        layer_id = rootf.name
-        logger.info(f"Searching for {layer_id}")
-        layer = Sdf.Layer.Find(layer_id)
-        if not layer:
-            logger.info(f"Layer {layer_id} was not found open. Attempting to open it.")
-            if not Sdf.Layer.FindOrOpen(layer_id):
-                logger.info(f"Layer {layer_id} does not exist on repository path: {resolver_ctx.GetSearchPath()}. Creating a new one.")
-                # we first create a layer under our repo
-                tmp_new_layer = Sdf.Layer.CreateNew(str(repo / layer_id))
-                # delete it since it will have an identifier with the full path,
-                # and we want to have the identifier relative to the repository path
-                # TODO: with AR 2.0 it should be possible to create in memory layers
-                #   with relative identifers to that of the search path of the context.
-                #   In the meantime, we need to create the layer first on disk.
-                del tmp_new_layer
-            stage = Usd.Stage.Open(layer_id)
-            logger.info(f"Root layer: {stage.GetRootLayer()}")
-            logger.info(f"Opened stage: {stage}")
-            cache_id = cache.Insert(stage)
-            logger.info(f"Added stage for {layer_id} with cache ID: {cache_id.ToString()}.")
-        else:
-            logger.info(f"Layer was open. Found: {layer}")
-            stage = cache.FindOneMatching(layer)
-            if not stage:
-                logger.info(f"Could not find stage on the cache.")
-                stage = Usd.Stage.Open(layer)
-                cache_id = cache.Insert(stage)
-                logger.info(f"Added stage for {layer} with cache ID: {cache_id.ToString()}.")
-            else:
-                logger.info(f"Found stage: {stage}")
-
-    return stage
-
-
-stage = fetch_stage(dracula_root_id)
+stage = easyedb.fetch_stage(dracula_root_id)
 tos = lambda: logger.info(stage.GetRootLayer().ExportToString())
 
-assert stage is fetch_stage(dracula_root_id)
+assert stage is easyedb.fetch_stage(dracula_root_id)
 
 # types, types.
 # this types should ideally come directly from EdgeDB? without reaching the database first?
-
-db_root_path = Sdf.Path("/DBTypes")
-
-db_tokens = types.MappingProxyType(dict(kingdom="db", item='types'))
-
-
-def define_db_type(stage, name, references=tuple()) -> Usd.Prim:
-    db_type_path = db_root_path.AppendChild(name)
-    db_type = stage.GetPrimAtPath(db_type_path)
-    if db_type:
-        return db_type
-
-    stage_layer = stage.GetRootLayer()
-    current_asset_name = UsdFile(Path(stage.GetRootLayer().realPath).name)
-    db_asset_name = current_asset_name.get(**db_tokens)
-    db_stage = fetch_stage(db_asset_name)
-
-    db_layer = db_stage.GetRootLayer()
-    if db_layer not in stage.GetLayerStack():
-        # TODO: There's a slight chance that the identifier is not a relative one.
-        #   Ensure we don't author absolute paths here. It should all be relative
-        #   to a path in our search path from the current resolver context.
-        #   If it's not happening, we need to manually create a relative asset path
-        #   str(Path(db_layer.identifier).relative_to(repo))
-        stage_layer.subLayerPaths.append(db_layer.identifier)
-
-    # Use class prims since we want db types to be abstract.
-    db_type = db_stage.CreateClassPrim(db_type_path)
-    for reference in references:
-        db_type.GetReferences().AddInternalReference(reference.GetPath())
-
-    if not db_stage.GetDefaultPrim():
-        db_stage.SetDefaultPrim(db_stage.GetPrimAtPath(db_root_path))
-    return stage.GetPrimAtPath(db_type_path)
-
-
-def _first_matching(tokens, layers):
-    tokens = set(tokens.items())
-    for layer in layers:
-        name = UsdFile(Path(layer.realPath).name)
-        if tokens.difference(name.values.items()):
-            continue
-        return layer
-    raise ValueError(f"Could not find layer matching {tokens}")
-
 
 # TODO: what should person and place be? Assemblies vs components.
 #   For now, only cities are considered assemblies.
 
 # all DB definitions go to the db types asset.
-displayable_type = define_db_type(stage, "DisplayableName")
-transport_enum = define_db_type(stage, "Transport")
-person_type = define_db_type(stage, "Person", (displayable_type,))
-pc_type = define_db_type(stage, "PC", (person_type, transport_enum))
-npc_type = define_db_type(stage, "NPC", (person_type,))
-vampire_type = define_db_type(stage, "Vampire", (person_type,))
-place_type = define_db_type(stage, "Place", (displayable_type,))
-city_type = define_db_type(stage, "City", (place_type,))
+displayable_type = easyedb.define_db_type(stage, "DisplayableName")
+transport_enum = easyedb.define_db_type(stage, "Transport")
+person_type = easyedb.define_db_type(stage, "Person", (displayable_type,))
+pc_type = easyedb.define_db_type(stage, "PC", (person_type, transport_enum))
+npc_type = easyedb.define_db_type(stage, "NPC", (person_type,))
+vampire_type = easyedb.define_db_type(stage, "Vampire", (person_type,))
+place_type = easyedb.define_db_type(stage, "Place", (displayable_type,))
+country_type = easyedb.define_db_type(stage, "Country", (place_type,))
+city_type = easyedb.define_db_type(stage, "City", (place_type,))
 
 # TODO: the following db relationships as well. This time we do this with an edit target
-db_layer = _first_matching(db_tokens, stage.GetLayerStack())
+db_layer = easyedb._first_matching(easyedb.DB_TOKENS, stage.GetLayerStack())
 
 ### DB edits  ###
-with Usd.EditContext(stage, db_layer):
+with easyedb.edit_context(db_layer, stage):
     displayable_type.CreateAttribute("display_name", Sdf.ValueTypeNames.String)
     variant_set = transport_enum.GetVariantSets().AddVariantSet("Transport")
     for set_name in ("Feet", "Train", "HorseDrawnCarriage"):
         variant_set.AddVariant(set_name)
 
-    person_type.CreateAttribute('age', Sdf.ValueTypeNames.Int2)
     # TODO: how to add constraints? Useful to catch errors before they hit the database
     #   https://github.com/edgedb/easy-edgedb/blob/master/chapter3/index.md#adding-constraints
+    person_type.CreateAttribute('age', Sdf.ValueTypeNames.Int2)
     person_type.CreateRelationship('places_visited')
 
     place_type.CreateAttribute("modern_name", Sdf.ValueTypeNames.String)
-    country_type = define_db_type(stage, "Country", (place_type,))
     for each in (city_type, country_type):
         # all places that end up in the database are "important places"
         Usd.ModelAPI(each).SetKind(Kind.Tokens.assembly)
@@ -161,61 +63,27 @@ with Usd.EditContext(stage, db_layer):
 pseudoRootPath = stage.GetPseudoRoot().GetPath()
 cityRoot = stage.DefinePrim(f"/{city_type.GetName()}")
 
+munich = easyedb.create(stage, city_type, 'Munich')
+budapest = easyedb.create(stage, city_type, 'Budapest', display_name='Buda-Pesth')
+bistritz = easyedb.create(stage, city_type, 'Bistritz', display_name='Bistritz')
 
-def create(stage, dbtype, name, display_name=""):
-    """Whenever we create a new item from the database, make it it's own entity"""
-    new_tokens = dict(kingdom='assets', cluster=dbtype.GetName(), item=name)
-    # contract: all dbtypes have a display_name
-    current_asset_name = UsdFile(Path(stage.GetRootLayer().realPath).name)
-    new_asset_name = current_asset_name.get(**new_tokens)
-
-
-    # Scope collecting all assets of the same type
-    scope_path = pseudoRootPath.AppendPath(dbtype.GetName())
-    scope = stage.GetPrimAtPath(scope_path)
-    if not scope:
-        scope = stage.DefinePrim(scope_path)
-    if not scope.IsModel():
-        Usd.ModelAPI(scope).SetKind(Kind.Tokens.assembly)
-    path = scope_path.AppendChild(name)
-    if stage.GetPrimAtPath(path):
-        return stage.GetPrimAtPath(path)
-
-    asset_stage = fetch_stage(new_asset_name)
-    asset_origin = asset_stage.GetPrimAtPath("/origin")
-    if not asset_origin:
-        asset_origin = asset_stage.DefinePrim()
-        asset_origin.GetReferences().AddReference(db_layer.identifier, dbtype.GetPath())
-
-    asset_stage.SetDefaultPrim(asset_origin)
-
-    if display_name:
-        asset_origin.GetAttribute("display_name").Set(display_name)
-
-    over_prim = stage.OverridePrim(path)
-    over_prim.GetPayloads().AddPayload(asset_stage.GetRootLayer().identifier)
-    return over_prim
-
-
-munich = create(stage, city_type, 'Munich')
-budapest = create(stage, city_type, 'Budapest', display_name='Buda-Pesth')
-bistritz = create(stage, city_type, 'Bistritz', display_name='Bistritz')
-
-bistritz_layer = _first_matching(
+bistritz_layer = easyedb._first_matching(
     dict(item='Bistritz', kingdom='assets'), (stack.layer for stack in bistritz.GetPrimStack())
 )
 
 # We need to explicitely construct our edit target since our layer is not on the layer stack of the stage.
-editTarget = Usd.EditTarget(bistritz_layer, bistritz.GetPrimIndex().rootNode.children[0])
-with Usd.EditContext(stage, editTarget):
+
+# editTarget = Usd.EditTarget(bistritz_layer, bistritz.GetPrimIndex().rootNode.children[0])
+# with Usd.EditContext(stage, editTarget):
+with easyedb.edit_context(bistritz, bistritz_layer, stage):
     bistritz.GetAttribute("modern_name").Set('Bistrița')
 
-hungary = create(stage, country_type, 'Hungary')
-romania = create(stage, country_type, 'Romania')
+hungary = easyedb.create(stage, country_type, 'Hungary')
+romania = easyedb.create(stage, country_type, 'Romania')
 
-jonathan = create(stage, pc_type, 'JonathanHarker', display_name='Jonathan Harker')
-emil = create(stage, pc_type, "EmilSinclair", display_name="Emil Sinclair")
-dracula = create(stage, vampire_type, 'CountDracula', display_name='Count Dracula')
+jonathan = easyedb.create(stage, pc_type, 'JonathanHarker', display_name='Jonathan Harker')
+emil = easyedb.create(stage, pc_type, "EmilSinclair", display_name="Emil Sinclair")
+dracula = easyedb.create(stage, vampire_type, 'CountDracula', display_name='Count Dracula')
 dracula.GetRelationship('places_visited').AddTarget(romania.GetPath())
 
 
@@ -246,19 +114,20 @@ for prim, places in {
 # we could set "important_places" as a custom new property
 # but "important" prims are already provided by the USD model hierarchy.
 # let's try it and see if we can get away with it.
-goldenKrone = create(stage, place_type, 'GoldenKroneHotel', 'Golden Krone Hotel')
+goldenKrone = easyedb.create(stage, place_type, 'GoldenKroneHotel', 'Golden Krone Hotel')
 # also, let's make it a child of bistritz
 childPrim = stage.OverridePrim(bistritz.GetPath().AppendChild(goldenKrone.GetName()))
 childPrim.GetReferences().AddInternalReference(goldenKrone.GetPath())
 Usd.ModelAPI(childPrim).SetKind(Kind.Tokens.component)  # should be component or reference?
 
-emil_layer = _first_matching(
+emil_layer = easyedb._first_matching(
     dict(item='EmilSinclair', kingdom='assets'), (stack.layer for stack in emil.GetPrimStack())
 )
 
 # We need to explicitely construct our edit target since our layer is not on the layer stack of the stage.
-editTarget = Usd.EditTarget(emil_layer, emil.GetPrimIndex().rootNode.children[0])
-with Usd.EditContext(stage, editTarget):
+# editTarget = Usd.EditTarget(emil_layer, emil.GetPrimIndex().rootNode.children[0])
+# with Usd.EditContext(stage, editTarget):
+with easyedb.edit_context(emil, emil_layer, stage):
     emil.GetVariantSet("Transport").SetVariantSelection("HorseDrawnCarriage")
 
 
@@ -275,7 +144,7 @@ if __name__ == "__main__":
     #
     # # for x in range(5_000):
     # for x in range(5):
-    #     create(stage, city_type, f'NewCity{x}', display_name=f"New City Hello {x}")
+    #     easyedb.create(stage, city_type, f'NewCity{x}', display_name=f"New City Hello {x}")
     #
     # stage.GetRootLayer().Save()
     stage.Save()
@@ -286,3 +155,5 @@ if __name__ == "__main__":
         for prim in city_root.GetChildren():
             logger.info(prim)
     persist(stage)
+    # code that uses 'var'; var.get() returns 'new value'. Call at the end.
+    easyedb.repo.reset(token)
