7623feda8a4a36b8189e92d71c9390c08cb05ea2
Christian López Barrón <chris.gfz@gmail.com>
2021-04-18 18:55:43 +1000

    Updated chapter 1 with grill write module

--- chapter1/usd.py
+++ chapter1/usd.py
@@ -7,34 +7,56 @@ Bistritz for example is now called Bistrița (it's in Romania), and Buda-Pesth i
 Some kind of Person type. We need it to have a name, and also a way to track the places that the person visited.
 
 """
-from grill import names
-from pxr import Usd, Sdf, Ar
-
-
-# class UsdFile(names.DateTimeFile, names.CGAssetFile):
-class UsdFile(names.CGAssetFile):
-    DEFAULT_SUFFIX = 'usda'
-
-
-from pathlib import Path
-repo = Path(__file__).parent / "assets"
 import logging
-logging.basicConfig(level=logging.INFO)
-LOG = logging.getLogger(__name__)
-usdf = UsdFile.get_default(code='dracula')
-LOG.info(f"Repository path: {repo}")
-LOG.info(f"Stage identifier: {usdf}")
-# we first create a layer under our repo
-layer = Sdf.Layer.CreateNew(str(repo / usdf.name))
-# delete it since it will have an identifier with the full path,
-# and we want to have the identifier relative to the repository path
-del layer
-# to get the relative identifier, use an asset resolver context to load the layer
-ctx = Ar.DefaultResolverContext([str(repo)])
-with Ar.ResolverContextBinder(ctx):
-    # stage's root layer identifier will now be relative to the repository path
-    stage = Usd.Stage.Open(usdf.name)
+from pathlib import Path
 
+from pxr import Sdf
+from grill import write
+
+repo = Path(__file__).parent / "assets"
+token = write.repo.set(repo)
+
+logging.basicConfig(level=logging.INFO)
+logging.getLogger("grill").setLevel(logging.INFO)
+logger = logging.getLogger(__name__)
+root_asset = write.UsdAsset.get_default(code='dracula')
+
+logger.info(f"Repository path: {repo}")
+logger.info(f"Stage identifier: {root_asset}")
+
+root_stage = write.fetch_stage(root_asset)
+
+displayable_type = write.define_db_type(root_stage, "DisplayableName")
+
+# TODO: the following db relationships as well. This time we do this with an edit target
+db_layer = write.find_layer_matching(write._DB_TOKENS, root_stage.GetLayerStack())
+city_type = write.define_db_type(root_stage, "City", (displayable_type,))
+person_type = write.define_db_type(root_stage, "Person", (displayable_type,))
+
+### DB edits  ###
+with write.edit_context(db_layer, root_stage):
+    displayable_type.CreateAttribute("display_name", Sdf.ValueTypeNames.String)
+    city_type.CreateAttribute("modern_name", Sdf.ValueTypeNames.String)
+    person_type.CreateRelationship('places_visited')
+
+munich = write.create(root_stage, city_type, 'Munich')
+budapest = write.create(root_stage, city_type, 'Budapest', display_name='Buda-Pesth')
+bistritz = write.create(root_stage, city_type, 'Bistritz', display_name='Bistritz')
+
+bistritz_layer = write.find_layer_matching(
+        dict(item='Bistritz', kingdom='assets'), (stack.layer for stack in bistritz.GetPrimStack())
+    )
+with write.edit_context(bistritz, bistritz_layer, root_stage):
+    bistritz.GetAttribute("modern_name").Set('Bistrița')
+
+
+budapest_layer = write.find_layer_matching(
+        dict(item='Budapest', kingdom='assets'), (stack.layer for stack in budapest.GetPrimStack())
+    )
+with write.edit_context(budapest, budapest_layer, root_stage):
+    budapest.GetAttribute("modern_name").Set('Budapest!')
+
+jonathan = write.create(root_stage, person_type, 'JonathanHarker', display_name='Jonathan Harker')
 
 """
 
@@ -58,64 +80,28 @@ INSERT Person {
 };
 """
 
-# types, types.
-# this types should ideally come directly from EdgeDB? without reaching the database first?
-from dataclasses import dataclass
-
-@dataclass
-class City:
-    name: str
-    display_name: str = ''
-    modern_name: str = ''
-
-@dataclass
-class Person:
-    name: str
-    display_name: str = ''
-
-munich = City('Munich')
-budapest = City('Budapest', 'Buda-Pesth', 'Budapest',)
-bistritz = City('Bistritz', 'Bistritz', 'Bistrița',)
-
-tos = lambda: print(stage.GetRootLayer().ExportToString())
-jonathan = Person('JonathanHarker', 'Jonathan Harker')
-
-cityPath = Sdf.Path("/City")
-personPath = Sdf.Path("/Person")
-for rootPath, children in {
-    cityPath: (munich, budapest, bistritz),
-    personPath: (jonathan,),
-}.items():
-    for each in children:
-        path = rootPath.AppendChild(each.name)
-        prim = stage.DefinePrim(path)
-        for datum in ('display_name', 'modern_name'):
-            value = getattr(each, datum, None)
-            if not value:
-                continue
-            prim.SetCustomDataByKey(datum, value)
+tos = lambda: print(root_stage.GetRootLayer().ExportToString())
 
 """
 If you just want to return a single part of a type without the object structure, you can use . after the type name. For example, SELECT City.modern_name will give this output:
 
 {'Budapest', 'Bistrița'}
 """
-
-cityRoot = stage.GetPrimAtPath(cityPath)
-print([p for p in Usd.PrimRange(cityRoot) if p.GetCustomDataByKey("modern_name")])
+cityRoot = budapest.GetParent()
+from pxr import Usd
+print([p for p in Usd.PrimRange(cityRoot) if p.GetAttribute("modern_name").Get()])
 # [Usd.Prim(</City/Budapest>), Usd.Prim(</City/Bistritz>)]
 
 """
 But we want to have Jonathan be connected to the cities he has traveled to. We'll change places_visited when we INSERT to places_visited := City:
 """
 
-jonathanPrim = stage.GetPrimAtPath(personPath).GetPrimAtPath(jonathan.name)
-jonathanVisitRel = jonathanPrim.CreateRelationship('places_visited')
+jonathanVisitRel = jonathan.GetRelationship('places_visited')
 for city in cityRoot.GetChildren():
     jonathanVisitRel.AddTarget(city.GetPath())
 
 tos()
-stage.GetRootLayer().Save()
+root_stage.Save()
 
 if __name__ == "__main__":
     ...
