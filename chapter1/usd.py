7f58251228bb88f4a3003190ffca750dd1f2353c
chrizzftd <chris.gfz@gmail.com>
2021-06-10 20:41:46 +1000

    Chapters 1-4: Use grill.write API (#1)

--- chapter1/usd.py
+++ chapter1/usd.py
@@ -7,56 +7,28 @@ Bistritz for example is now called Bistrița (it's in Romania), and Buda-Pesth i
 Some kind of Person type. We need it to have a name, and also a way to track the places that the person visited.
 
 """
-import logging
 from pathlib import Path
 
-from pxr import Sdf
 from grill import write
+from pxr import Sdf, Usd
 
-repo = Path(__file__).parent / "assets"
-token = write.repo.set(repo)
+write.repo.set(Path(__file__).parent / "assets")
 
-logging.basicConfig(level=logging.INFO)
-logging.getLogger("grill").setLevel(logging.INFO)
-logger = logging.getLogger(__name__)
-root_asset = write.UsdAsset.get_default(code='dracula')
+stage = write.fetch_stage(write.UsdAsset.get_default(code='dracula'))
 
-logger.info(f"Repository path: {repo}")
-logger.info(f"Stage identifier: {root_asset}")
+# we can define a category with or without an edit context
+city = write.define_taxon(stage, "City")
 
-root_stage = write.fetch_stage(root_asset)
+with write.taxonomy_context(stage):
+    person = write.define_taxon(stage, "Person")
+    # but to edit a category definition we must be in the proper context
+    city.CreateAttribute("modern_name", Sdf.ValueTypeNames.String)
+    person.CreateRelationship('places_visited')
 
-displayable_type = write.define_db_type(root_stage, "DisplayableName")
-
-# TODO: the following db relationships as well. This time we do this with an edit target
-db_layer = write.find_layer_matching(write._DB_TOKENS, root_stage.GetLayerStack())
-city_type = write.define_db_type(root_stage, "City", (displayable_type,))
-person_type = write.define_db_type(root_stage, "Person", (displayable_type,))
-
-### DB edits  ###
-with write.edit_context(db_layer, root_stage):
-    displayable_type.CreateAttribute("display_name", Sdf.ValueTypeNames.String)
-    city_type.CreateAttribute("modern_name", Sdf.ValueTypeNames.String)
-    person_type.CreateRelationship('places_visited')
-
-munich = write.create(root_stage, city_type, 'Munich')
-budapest = write.create(root_stage, city_type, 'Budapest', display_name='Buda-Pesth')
-bistritz = write.create(root_stage, city_type, 'Bistritz', display_name='Bistritz')
-
-bistritz_layer = write.find_layer_matching(
-        dict(item='Bistritz', kingdom='assets'), (stack.layer for stack in bistritz.GetPrimStack())
-    )
-with write.edit_context(bistritz, bistritz_layer, root_stage):
-    bistritz.GetAttribute("modern_name").Set('Bistrița')
-
-
-budapest_layer = write.find_layer_matching(
-        dict(item='Budapest', kingdom='assets'), (stack.layer for stack in budapest.GetPrimStack())
-    )
-with write.edit_context(budapest, budapest_layer, root_stage):
-    budapest.GetAttribute("modern_name").Set('Budapest!')
-
-jonathan = write.create(root_stage, person_type, 'JonathanHarker', display_name='Jonathan Harker')
+write.create(city, 'Munich')
+budapest = write.create(city, 'Budapest', label='Buda-Pesth')
+bistritz = write.create(city, 'Bistritz', label='Bistritz')
+jonathan = write.create(person, 'JonathanHarker', label='Jonathan Harker')
 
 """
 
@@ -71,7 +43,7 @@ INSERT City {
 
 INSERT City {
   name := 'Bistritz',
-  modern_name := 'Bistri?a'
+  modern_name := 'Bistrița'
 };
 
 INSERT Person {
@@ -80,16 +52,19 @@ INSERT Person {
 };
 """
 
-tos = lambda: print(root_stage.GetRootLayer().ExportToString())
+with write.unit_context(bistritz):
+    bistritz.GetAttribute("modern_name").Set('Bistrița')
+
+with write.unit_context(budapest):
+    budapest.GetAttribute("modern_name").Set('Budapest!')
 
 """
 If you just want to return a single part of a type without the object structure, you can use . after the type name. For example, SELECT City.modern_name will give this output:
 
 {'Budapest', 'Bistrița'}
 """
-cityRoot = budapest.GetParent()
-from pxr import Usd
-print([p for p in Usd.PrimRange(cityRoot) if p.GetAttribute("modern_name").Get()])
+city_root = stage.GetPseudoRoot().GetPrimAtPath(city.GetName())
+print([p for p in Usd.PrimRange(city_root) if p.GetAttribute("modern_name").Get()])
 # [Usd.Prim(</City/Budapest>), Usd.Prim(</City/Bistritz>)]
 
 """
@@ -97,11 +72,10 @@ But we want to have Jonathan be connected to the cities he has traveled to. We'l
 """
 
 jonathanVisitRel = jonathan.GetRelationship('places_visited')
-for city in cityRoot.GetChildren():
+for city in city_root.GetChildren():
     jonathanVisitRel.AddTarget(city.GetPath())
 
-tos()
-root_stage.Save()
+stage.Save()
 
 if __name__ == "__main__":
     ...
