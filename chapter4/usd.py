f69c29eded69989a839cca275456e5b6c2d4e6bc
Christian López Barrón <chris.gfz@gmail.com>
2025-10-19 00:08:50 +1100

    updating grill

--- chapter4/usd.py
+++ chapter4/usd.py
@@ -76,15 +76,16 @@ def main():
 
     {'Budapest', 'Bistrița'}
     """
-    print([p for p in cook.itaxa(stage.Traverse(), city) if p.GetAttribute("modern_name").Get()])
+    cities = cook.filter_taxa(stage.Traverse(), city)
+    print([p for p in cities if p.GetAttribute("modern_name").Get()])
     # [Usd.Prim(</City/Budapest>), Usd.Prim(</City/Bistritz>)]
 
     """
     But we want to have Jonathan be connected to the cities he has traveled to. We'll change places_visited when we INSERT to places_visited := City:
     """
     for person, places in {
-        jonathan: cook.itaxa(stage.Traverse(), city),
-        emil: cook.itaxa(stage.Traverse(), city),
+        jonathan: cities,
+        emil: cities,
         dracula: [romania],
         mina: [london],
     }.items():
@@ -104,12 +105,11 @@ def main():
     # 4 Questions / Unresolved
     # Time type, needed? Has "awake" property  driven by hour ( awake < 7am asleep < 19h awake
 
-    # # for x in range(1_000):
-    # for x in range(10):
-    #     # atm creating 1_000 new cities (including each USD file) takes around 7 seconds.
-    #     # Total time: 0:00:06.993190
-    #     # could be faster.
-    #     cook.create_unit(city, f'NewCity{x}', label=f"New City Hello {x}")
+    for x in range(10):
+        # atm creating 1_000 new cities (including each USD file) takes around 7 seconds.
+        # Total time: 0:00:05.535153
+        # could be faster.
+        cook.create_unit(city, f'NewCity{x}', label=f"New City Hello {x}")
 
     stage.Save()
     cook.Repository.reset(token)
